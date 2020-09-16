a167b6d5776557847926d3a914146a5a2ed5baa7
makkiboukhris <makki01boukhris@gmail.com>
2020-09-16 17:18:27 +0100

    Updates

---
+++ precache-manifest.c56a9dd922667b35a9ae5da2941e1d29.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f19c8ffaa950e75dbe82d46aac506513",
+    "url": "/reactt11/index.html"
+  },
+  {
+    "revision": "03717c3e158c4ce21180",
+    "url": "/reactt11/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "0fb453958ef25144d4d0",
+    "url": "/reactt11/static/css/main.a57be869.chunk.css"
+  },
+  {
+    "revision": "03717c3e158c4ce21180",
+    "url": "/reactt11/static/js/2.b7620af2.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/reactt11/static/js/2.b7620af2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0fb453958ef25144d4d0",
+    "url": "/reactt11/static/js/main.03658375.chunk.js"
+  },
+  {
+    "revision": "18fcb3474a6dadab2c4a",
+    "url": "/reactt11/static/js/runtime-main.7a30d537.js"
+  }
+]);
